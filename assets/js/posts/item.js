c3f75679da1a594c4044239099d2b8406021d7c8
prabirshrestha <mail@prabir.me>
2014-01-29 22:57:36 -0500

    footer

--- assets/js/posts/item.js
+++ assets/js/posts/item.js
@@ -1,7 +1,20 @@
 /** @jsx React.DOM */
 var React = require('react'),
 
+    Footer,
     Item;
+
+Footer = React.createClass({
+    render: function () {
+        var post = this.props.post;
+        return (
+            <small>
+                {post.points} points by {post.postedBy + ' ' + post.postedAgo} |
+                { post.commentCount } { post.commentCount === 1 ? ' comment' : ' comments' }
+            </small>
+        );
+    }
+});
 
 Item = React.createClass({
     render: function () {
@@ -9,6 +22,7 @@ Item = React.createClass({
         return (
             <li>
                 <a href={post.url}>{post.title}</a>
+                <Footer post={post}/>
             </li>
         );
     }
